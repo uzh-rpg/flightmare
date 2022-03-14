63109d8122615f6dd374a5a6a23dc78523bad955
Yunlong Song <yun-long.song@outlook.com>
2022-03-14 19:38:10 +0100

    add setpy.py for rpg_baselines

---
+++ flightpy/flightrl/setup.py
@@ -0,0 +1,20 @@
+
+import os
+import re
+import sys
+import platform
+import subprocess
+
+from setuptools import setup, Extension, find_packages
+from setuptools.command.build_ext import build_ext
+from distutils.version import LooseVersion
+
+setup(
+    name='rpg_baselines',
+    version='0.0.1',
+    author='Yunlong Song',
+    author_email='song@ifi.uzh.ch',
+    description='Flightmare: A Quadrotor Simulator.',
+    long_description='',
+    packages=['rpg_baselines'],
+)
